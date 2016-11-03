efa70962e8f22f4a9900cd28b061fa1232784a7c
AngelaBoline <angelaboline@gmail.com>
2016-11-03 13:49:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$("document").ready(function() {
+	$("[data-type='vitamin']").css("background-color", "#12500F");
+	$("[data-type='mineralwater']").css("background-color", "#2982D0");
+	$("[data-type='proteinbar']").css("background-color", "#4E0F50");
+}
+	);
